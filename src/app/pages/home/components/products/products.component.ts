a65d32b05d2ee556ed0b9f33898385dab0243e76
FX Drooklyn <dr.f380@hotmail.com>
2023-12-17 20:17:19 +0100

    products

--- src/app/pages/home/components/products/products.component.ts
+++ src/app/pages/home/components/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
+  @Input() fullWidthMode = false;
+  
   constructor() {}
 
   ngOnInit(): void {
